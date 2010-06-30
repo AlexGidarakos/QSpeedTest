4ad9e46ef078016d44d1575e2cd6fe14280533d1
Alexandros Gidarakos <algida79@gmail.com>
2010-06-30 16:20:39 +0000

    revision 27 - 20100630                                              |     * Bumped QSpeedTest revision to r27                             |     * Bumped QSpeedTestCLI revision to r6                           |     * QSpeedTest r27: Fixed a nasty bug that crashed the program    |       when pressing for the 2nd time the stop button during a       |       download speed test                                           |     * QSpeedTestCLI r6: A small cosmetic fix in stdout output       |

--- main_qspeedtest.cpp
+++ main_qspeedtest.cpp
@@ -27,7 +27,7 @@ along with QSpeedTest.  If not, see <http://www.gnu.org/licenses/>.
 const QString PROJECTNAME = "QSpeedTest";
 const QString PROGRAMNAME = "QSpeedTest";
 const QString PROGRAMAUTHOR = "parsifal";
-const QString PROGRAMVERSION = "r25";
+const QString PROGRAMVERSION = "r27";
 const QString PROGRAMURL = "https://sourceforge.net/projects/qspeedtest/files/";
 const QString PROGRAMDISCUSSURL = "https://sourceforge.net/apps/phpbb/qspeedtest";
 const QString PROGRAMUPDATECHECKURL = "http://qspeedtest.sourceforge.net/updates/qspeedtest.version";
